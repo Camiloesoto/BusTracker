643798605b1693d3e6c00f6c756b3afea38ed4ac
PEDRO CARDENAS RESTREPO <pedrocardenasrestrepo@gmail.com>
2023-10-22 16:43:43 -0500

    Chatbot

--- busTracker/busTracker/urls.py
+++ busTracker/busTracker/urls.py
@@ -4,7 +4,9 @@ from cuentas import views as views_cuentas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views_cuentas.signupaccount),
+    path('register', views_cuentas.signupaccount),
     path('accounts/', include('cuentas.urls')),
-    path('bus/', include('bus.urls'))
+    path('bus/', include('bus.urls')),
+    path('', include('chatbot.urls')),
+
 ]
