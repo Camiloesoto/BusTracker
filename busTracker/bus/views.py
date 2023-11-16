1305420e4a3ea8c4aba6753e0c65c719d2f029aa
Camiloesoto <100038164+Camiloesoto@users.noreply.github.com>
2023-11-15 22:06:28 -0500

    Delete busTracker directory

--- busTracker/bus/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from . import utils
-
-def home(request):
-    origen = request.GET.get('origen')
-    destination = request.GET.get('destination')
-    time = utils.aproximated_time(origen, destination)
-    return render(request, 'home.html', {'time': time})
